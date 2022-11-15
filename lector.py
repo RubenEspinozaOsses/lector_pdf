945eae9547c67119aefbdf3fb8fbd7b2a6a0bb3b
Ruben Espinoza Osses <ruben.espinoza.osses@gmail.com>
2022-11-15 18:09:29 -0300

    Se opta por solucion PyPDF2

--- lector.py
+++ lector.py
@@ -1,37 +1,54 @@
 import tabula
+from PyPDF2 import PdfReader
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
-
-user_data = [11.138,31.025,165.668,580.295]
-glosa_1 = [169.493,37.91,376.808,304.895]
-glosa_2 = [167.963,308.72,376.043,574.94]
-pago = [377.573,34.85,503.798,426.53]
-datos_banco = [506.093,34.85,597.128,575.705]
 
 
-pdfs = [file for file in os.listdir(os.path.join('files')) if file.endswith('.pdf')]
+def identify_format(title):
+    match title:
+        case 'ACREDITAR TAMAÑO EMPRESA':
+            return 0
 
-def extract_datatables(regions, name, idx):
-    dir_name = name.split('.')[0]
-    if not os.path.exists(f'files/out/{dir_name}'):
-        os.mkdir(f'files/out/{dir_name}')
-    return tabula.io.read_pdf(f'files/{name}', pages='all', area=regions, output_format='json', stream=True, output_path=f'files/out/{dir_name}/table-{idx}.json')
+        case 'PERSONALIZADA':
+            return 1
 
+        case 'SOLICITAR CRÉDITOS':
+            return 2
 
+        case 'ACREDITAR RENTA':
+            return 3
 
-for index, pdf in enumerate(pdfs):
-    user_data_table = extract_datatables(user_data, pdf, index)
-    glosa_1_table = extract_datatables(glosa_1, pdf, index)
-    glosa_2_table = extract_datatables(glosa_2, pdf, index)
-    pago_table = extract_datatables(pago, pdf, index)
-    datos_banco_table = extract_datatables(datos_banco, pdf, index)
+def get_title_of_pdf(pdf):
+    reader = PdfReader(pdf)
+    page = reader.pages[0]
+    text = page.extract_text()
 
+    unclean_title = text.split('\n')[1]
+
+    if ' ' in unclean_title:
+        return ' '.join(unclean_title.split(' ')[1::])
+    else:
+        return unclean_title
+
+def get_tables_from_pdf(pdf, regions, page):
+    if page == 2: regions = [30, 581, 24, 36]
+    return tabula.io.read_pdf(pdf, area=regions, pages=page, stream=True, multiple_tables=False, format='JSON', output_path='1.json')
+
+def extract_data_from_tables(pdf):
+    format_code = identify_format(get_title_of_pdf(pdf))
+    regions = [30, 581, 12, 24]
+    print(get_tables_from_pdf(pdf, regions, 2))
+    print(get_tables_from_pdf(pdf, regions, 3))
     
-    
-    
 
 
+def extract_raw_data(pdf):
+    reader = PdfReader(pdf)
 
+    for page_num, page in enumerate(reader.pages):
 
+        text = page.extract_text()
+        print(f'|||||||||{page_num}|||||||||')
+        print(text.split('\n'))
 
+extract_raw_data('files/1.pdf')
---
+++ pasos.md
@@ -0,0 +1,9 @@
+# Pasos para leer pdfs
+
+## 1. Identificar formato de pdf [ACREDITAR TAMAÑO EMPRESA, PERSONALIZADA, SOLICITAR CREDITOS, ACREDITAR RENTA]
+
+## 2. De cada pdf:
+### a. Ubicar las tablas de acuerdo a su posicion
+### b. Extraer los datos de las tablas
+### c. Exportar las tablas a formato json
+
